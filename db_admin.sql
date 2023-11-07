2d1f9548e0b8c070e0ace3fbe641091d91b4ef33
yBCddsrs7Z <128252181+yBCddsrs7Z@users.noreply.github.com>
2023-11-07 18:05:58 +0100

    Update db_admin.sql

--- db_admin.sql
+++ db_admin.sql
@@ -84,11 +84,11 @@ CREATE TABLE IF NOT EXISTS `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id`, `name`, `username`, `email`, `password`, `mobile`, `roleid`, `isActive`, `created_at`, `updated_at`) VALUES
-(23, 'achref', 'achref', 'achref.nefzazoui@gmail.com', '3ea543d29ad3c1c09fcfbdda3f2f0617c50ab138', '54852852', 1, 0, '2020-12-19 14:35:56', '2020-12-19 14:35:56'),
-(24, 'ahmed', 'benahmed', 'achme@gmail.com', '7f0c9d56d40c3cc1e23e0113d5377779a4de86ff', '54277528', 3, 0, '2020-12-19 15:13:39', '2020-12-19 15:13:39'),
-(25, 'Fathi', 'fathiA', 'fathianh@gmail.com', '0a859b9a4ebbde4f63383bca7e34890985782348', '54672828', 3, 0, '2020-12-19 15:15:52', '2020-12-19 15:15:52'),
-(26, 'Makrem', 'makrem', 'makrem@gmail.com', 'adef7009a84a71c226ddf68671e929d68a707762', '42551771', 3, 0, '2020-12-19 15:16:59', '2020-12-19 15:16:59'),
-(27, 'Sirin', 'Sirin', 'Sirin@gmail.com', '03ee5fda2eae80be34c0142fe28ac6401e63324c', '23451671', 3, 0, '2020-12-19 15:17:34', '2020-12-19 15:17:34');
+(23, 'achref', 'achref', 'achref.nefzazoui@gmail.com', sha1('Achref1'), '54852852', 1, 0, '2020-12-19 14:35:56', '2020-12-19 14:35:56'),
+(24, 'ahmed', 'benahmed', 'achme@gmail.com', sha1('Achref1'), '54277528', 3, 0, '2020-12-19 15:13:39', '2020-12-19 15:13:39'),
+(25, 'Fathi', 'fathiA', 'fathianh@gmail.com', sha1('Achref1'), '54672828', 3, 0, '2020-12-19 15:15:52', '2020-12-19 15:15:52'),
+(26, 'Makrem', 'makrem', 'makrem@gmail.com', sha1('Achref1'), '42551771', 3, 0, '2020-12-19 15:16:59', '2020-12-19 15:16:59'),
+(27, 'Sirin', 'Sirin', 'Sirin@gmail.com', sha1('Achref1'), '23451671', 3, 0, '2020-12-19 15:17:34', '2020-12-19 15:17:34');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
